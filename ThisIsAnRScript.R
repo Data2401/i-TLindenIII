3fdb00e09b57a47fec639424494df2c809c78c2c
Thomas Linden <117541579+TLindenIII@users.noreply.github.com>
2025-01-16 13:07:41 -0600

    Fixed comment, plotted sqrt with formatting

--- ThisIsAnRScript.R
+++ ThisIsAnRScript.R
@@ -1,9 +1,12 @@
 ## This is a R script 
-## Run this code to make a plot of x, x^3, and x^(1/2) for the integers from 1 to 10
+## Run this code to make a plot of x, x^3, and x^(1/2) for the integers from 1 to 20
 
 x <- 1:20
 y <- x^3
 z <- x^(1/2)
 
-plot(x,y, type = "b", main = "Cubed integers")
-plot(x,z, type = "b", main = "Square roots of integers")
+plot(x,y, type = "b", main = "Cubed and squared integers", col = "blue", xlab = "x", ylab = "y and z")
+lines(x,z, type = "b", main = "Square roots of integers", col = "red")
+
+legend("topleft", legend = c("Cubed Integers", "Square Roots"),
+       col = c("blue","red"), lty = 1, pch = 1)
