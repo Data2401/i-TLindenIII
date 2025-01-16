4af073bffa82b7ea3e6e2cb4290549b7acf974bf
Thomas Linden <117541579+TLindenIII@users.noreply.github.com>
2025-01-16 12:49:45 -0600

    Add sqrt(x) plot to ThisIsAnRScript.R

--- ThisIsAnRScript.R
+++ ThisIsAnRScript.R
@@ -1,8 +1,9 @@
 ## This is a R script 
-## Run this code to make a plot of x and x^3 for the integers from 1 to 10
+## Run this code to make a plot of x, x^3, and x^(1/2) for the integers from 1 to 10
 
 x <- 1:20
 y <- x^3
-
+z <- x^(1/2)
 
 plot(x,y, type = "b", main = "Cubed integers")
+plot(x,z, type = "b", main = "Square roots of integers")
